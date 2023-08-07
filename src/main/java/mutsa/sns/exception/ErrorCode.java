1ad4c9c8346dab5ce887975440dd8e1f93fae49b
Shjung <wjdtngus9@gmail.com>
2023-08-07 15:09:48 +0900

    feat: 이미지 저장 관련 에러코드 추가

--- src/main/java/mutsa/sns/exception/ErrorCode.java
+++ src/main/java/mutsa/sns/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     PASSWORD_NOT_MATCH(HttpStatus.UNAUTHORIZED,"비밀번호가 일치하지 않습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, " 를(을) 찾을 수 없습니다."),
     IMAGE_NOT_FOUND(HttpStatus.BAD_REQUEST,"이미지를 첨부해주세요"),
-    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "사용자 권한이 없습니다.");
+    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "사용자 권한이 없습니다."),
+    DIRECTORY_EXISTS(HttpStatus.INTERNAL_SERVER_ERROR,"이미 해당 이름의 폴더가 존재합니다.");
 
     private HttpStatus status;
     String msg;
