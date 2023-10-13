20ed4465aa25a688e8f8e661224949ade36488cf
Shjung <wjdtngus9@gmail.com>
2023-10-13 23:07:52 +0900

    :recycle: refactor: Restful api 설계를 위한 수정

--- src/main/java/mutsa/sns/controller/ArticleController.java
+++ src/main/java/mutsa/sns/controller/ArticleController.java
@@ -2,16 +2,11 @@ package mutsa.sns.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import mutsa.sns.domain.dto.article.ArticleRequestDto;
 import mutsa.sns.domain.dto.article.ArticleResponseDto;
 import mutsa.sns.domain.dto.like.LikeResponseDto;
-import mutsa.sns.exception.CustomException;
-import mutsa.sns.exception.ErrorCode;
 import mutsa.sns.service.ArticleService;
 import mutsa.sns.service.LikeService;
-import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,58 +17,65 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/article")
+@RequestMapping("/articles")
 public class ArticleController {
     private final ArticleService articleService;
     private final LikeService likeService;
 
-    @PostMapping(value ="/{username}/write", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ArticleResponseDto write(@PathVariable String username, Authentication auth,
-                                    @RequestParam(value = "image", required = false)  List<MultipartFile> image,
+    /**
+     * 게시글 등록
+     */
+    @PostMapping(value = "/write", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ArticleResponseDto write(Authentication auth,
+                                    @RequestParam(value = "image", required = false) List<MultipartFile> image,
                                     @RequestParam String title,
                                     @RequestParam String content) throws IOException {
 
-        if(!auth.getName().matches(username)){
-            log.info("auth.getName() = {} ,username = {}",auth.getName(),username );
-            throw new CustomException(ErrorCode.INVALID_PERMISSION);
-        }
-
-        return articleService.write(username, image, title , content);
+        return articleService.write(auth.getName(), image, title, content);
     }
 
+    /**
+     * 게시글 조회
+     */
     @GetMapping("/read/{username}/{articleId}")
     public ArticleResponseDto read(@PathVariable(value = "username") String username,
-                                   @PathVariable(value = "articleId") Integer articleId){
+                                   @PathVariable(value = "articleId") Integer articleId) {
 
         return articleService.readArticle(username, articleId);
     }
 
     @GetMapping("/read/{username}")
-    public List<ArticleResponseDto> readSaleItemAll(@PathVariable(value = "username") String username){
+    public List<ArticleResponseDto> readSaleItemAll(@PathVariable(value = "username") String username) {
         return articleService.readAll(username);
     }
 
-    @DeleteMapping("/delete/{username}/{articleId}")
-    public void delete(@PathVariable String username,Authentication auth,
-                        @PathVariable(value = "articleId") Integer articleId) throws IOException {
+    /**
+     * 게시글 수정
+     */
+    @PutMapping(value = "/{articleId}/update", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ArticleResponseDto update(@PathVariable(value = "articleId") Integer articleId,
+                                     Authentication auth,
+                                     @RequestParam(value = "image", required = false) List<MultipartFile> image,
+                                     @RequestParam String title,
+                                     @RequestParam String content) throws IOException {
 
-        if(!auth.getName().matches(username)){
-            log.info("auth.getName() = {} ,username = {}",auth.getName(),username );
-            throw new CustomException(ErrorCode.INVALID_PERMISSION);
-        }
-
-        articleService.delete(username, articleId);
+        return articleService.updateArticle(articleId, auth.getName(), title, content, image);
     }
 
-    @PutMapping("/read/{username}/{articleId}/like")
-    public LikeResponseDto like(@PathVariable String username,Authentication auth,
-                                @PathVariable(value = "articleId") Integer articleId){
+    @DeleteMapping("/{articleId}/delete/")
+    public void delete(Authentication auth,
+                       @PathVariable(value = "articleId") Integer articleId) throws IOException {
 
-        if(auth.getName().matches(username)){
-            throw new CustomException(ErrorCode.INVALID_PERMISSION);
-        }
+        articleService.delete(auth.getName(), articleId);
+    }
 
-        return likeService.like(auth.getName(), articleId);
+    /**
+     * 해당 게시글을 좋아한 유저 목록 보기
+     * */
+    @PutMapping(value = "/{articleId}/like-user")
+    public LikeResponseDto likeUserList(@PathVariable(value = "articleId") Integer articleId) {
+
+        return likeService.likeUserList(articleId);
     }
 
 }
