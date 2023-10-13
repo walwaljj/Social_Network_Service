4f46f376481b7cd86a53a3da236962af76f51466
Shjung <wjdtngus9@gmail.com>
2023-10-13 22:56:14 +0900

    :bug: fix: UserRepository Identity 타입변경

--- src/main/java/mutsa/sns/repository/UserRepository.java
+++ src/main/java/mutsa/sns/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<UserEntity , Long> {
+public interface UserRepository extends JpaRepository<UserEntity , Integer> {
     Optional<UserEntity> findByUsername(String username);
     boolean existsByUsername(String username);
 }
